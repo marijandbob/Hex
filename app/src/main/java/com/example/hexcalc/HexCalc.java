9253e96d774799ff364bc207f602fadfe2850eb3
marijandbob <bplotts@optonline.com>
2015-08-06 23:10:58 -0400

    minor change

--- app/src/main/java/com/example/hexcalc/HexCalc.java
+++ app/src/main/java/com/example/hexcalc/HexCalc.java
@@ -14,7 +14,7 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class HexCalc extends ActionBarActivity {
-    final String TAG = "HexCalc"; 
+    final String TAG = "HexCalc";
 
 
     @Override
